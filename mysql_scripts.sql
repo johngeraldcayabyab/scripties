3eda8230052278163d8dd39be1e9ba8d91075250
johngeraldcayabyab <johngeraldcayabyab@gmail.com>
2022-12-15 09:17:22 +0800

    Add new script

--- mysql_scripts.sql
+++ mysql_scripts.sql
@@ -1,2 +1,5 @@
 #Show rows from same table
 SELECT id FROM table_name WHERE duplicate_column IN ( SELECT duplicate_column FROM table_name GROUP BY duplicate_column HAVING COUNT(*) > 1) ORDER BY duplicate_column DESC;
+
+#Duplicate delete where location_id and datetime defines the uniqueness of a row
+ALTER IGNORE TABLE table_name ADD UNIQUE (location_id, datetime)
