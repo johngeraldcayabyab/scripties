acfaa05ce2ddf4775018328913f3c6861915bf48
Dlareg <johngeraldcayabyab@gmail.com>
2022-12-29 09:14:48 +0800

    Added new script that checks db size in mb

--- mysql_scripts.sql
+++ mysql_scripts.sql
@@ -3,3 +3,6 @@ SELECT id FROM table_name WHERE duplicate_column IN ( SELECT duplicate_column FR
 
 #Duplicate delete where location_id and datetime defines the uniqueness of a row
 ALTER IGNORE TABLE table_name ADD UNIQUE (location_id, datetime)
+
+#Check db size in MB
+SELECT table_schema "db_name", ROUND(SUM(data_length + index_length) / 1024 / 1024, 1) "DB Size in MB" FROM information_schema.tables GROUP BY table_schema;
